cee60247d69b726c60179293be016f943e034b22
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-12-16 10:10:20 +0100

    done hw

--- 01_UDP/01_server/Program.cs
+++ 01_UDP/01_server/Program.cs
@@ -8,6 +8,7 @@ namespace _01_server
     {
         const int port = 3300;
 
+        
         static void Main(string[] args)
         {
             // створюємо UDP сокет для відправки та отримання пакетів даних
@@ -27,26 +28,85 @@ namespace _01_server
                 Console.WriteLine($"Request: {message} at {DateTime.Now} from {clientIP}");
                 Console.ResetColor();
 
-
-
                 // відправляємо відповідь клієнту
-                server.Send(Encoding.UTF8.GetBytes("OK"), clientIP);
+                Reply reply = new Reply();
+                server.Send(Encoding.UTF8.GetBytes(reply.ReplyToRequest(message)), clientIP);
             }
         }
     }
-    public class GetAnswerToRequest
+    enum Answers
     {
-        private static string hello = "Hello!";
-        private static string bye = "Bye!";
-        private static string another = "I received your message";
-        public static string GetAnswer(string request)
+        hi,
+        bye,
+        who_are_you,
+        fact,
+    }
+    public class Reply
+    {
+        public string ReplyToRequest(string message)
         {
-            if (request == "Hello" || request == "Hi")
-                return hello;
-            else if (request == "Good bye" || request == "Bye")
-                return bye;
-            else
-                return another;
+            string reply = string.Empty;
+            message = message.ToLower();
+            Answers type_answer = new Answers();
+            //списки з запитаннями
+            List<string> msg_hi = new List<string> { "hi", "hello", "hola", "whats up", "privet" };
+            List<string> msg_bye = new List<string> { "bye", "good bye", "chao", "paka" };
+            List<string> msg_whoAreYou = new List<string> { "who are you", "what can you do", "what are you made for" };
+            List<string> msg_fact = new List<string> { "fact", "tell me the fact", "interesting fact" };
+
+            //оприділення запитання
+            foreach (string msg in msg_hi)
+            {
+                if (msg == message)
+                {
+                    type_answer = Answers.hi;
+                    break;
+                }
+            }
+            foreach (string msg in msg_bye)
+            {
+                if (msg == message)
+                {
+                    type_answer = Answers.bye;
+                    break;
+                }
+            }
+            foreach (string msg in msg_whoAreYou)
+            {
+                if (msg == message)
+                {
+                    type_answer = Answers.who_are_you;
+                    break;
+                }
+            }
+            foreach (string msg in msg_fact)
+            {
+                if (msg == message)
+                {
+                    type_answer = Answers.fact;
+                    break;
+                }
+            }
+
+            //генерування відповіді
+            if(type_answer == Answers.hi)
+            {
+                reply = "Hello, I'm Server!";
+            }
+            if (type_answer == Answers.bye)
+            {
+                reply = "Bye, glad to help";
+            }
+            if (type_answer == Answers.who_are_you)
+            {
+                reply = "I am your server created to answer some of your questions, my name is GOVORUN";
+            }
+            if (type_answer == Answers.fact)
+            {
+                reply = "I'm made for homework";
+            }
+
+            return reply;
         }
     }
 }
