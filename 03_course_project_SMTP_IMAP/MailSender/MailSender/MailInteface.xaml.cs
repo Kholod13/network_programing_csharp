22b46e3b6220d1632a887b0f2250d2f01f9abae7
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-12-25 02:32:57 +0100

    made changes

--- 03_course_project_SMTP_IMAP/MailSender/MailSender/MailInteface.xaml.cs
+++ 03_course_project_SMTP_IMAP/MailSender/MailSender/MailInteface.xaml.cs
@@ -13,6 +13,7 @@ using MimeKit;
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Controls;
 
 namespace MailSender
 {
@@ -35,29 +36,48 @@ namespace MailSender
             {
                 client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
 
-                client.Authenticate(userLogin, UserPassword);
+                client.Authenticate(UserLogin, UserPassword);
 
                 // ------------------------------------------------------------------------------
 
                 // --------------- get all folders
+
+                
                 foreach (var item in client.GetFolders(client.PersonalNamespaces[0]))
                 {
                     FolderList.Items.Add(item.Name);
                 }
 
-                // -------------- get all sent messages
+                if (FolderList.SelectedItem != null)
+                {
+                    /*
+                    var uids = client.Inbox.Search(SearchQuery.Seen);
+                    MessagesList.Items.Add($"Folder");
+                    foreach (var uid in uids)
+                    {
+                        var message = client.Inbox.GetMessage(uid);
+                        MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.Take(10).ToArray())}...");
+                    }
+                    */
+                    var selectedText = (FolderList.SelectedItem as ListBoxItem).Content.ToString();
+                    MessageBox.Show($"Ви обрали: {selectedText}");
+                }
 
+                // -------------- get all sent messages
+                /*
                 var folder = client.GetFolder(MailKit.SpecialFolder.Sent);
                 folder.Open(FolderAccess.ReadWrite);
 
-                IList<MailKit.UniqueId> uids = folder.Search(SearchQuery.All);
+                IList<MailKit.UniqueId> uids = folder.Search(SearchQuery.Seen);
                 
-                MessagesList.Items.Add("--------- Sent Mailbox:");
                 foreach (var i in uids)
                 {
                     MimeMessage message = folder.GetMessage(i);
                     MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.Take(10).ToArray())}...");
                 };
+                */
+
+
 
                 //// -------------------- show Inbox 
                 /*
@@ -93,8 +113,35 @@ namespace MailSender
             
 
         }
-        
 
+        private void FolderList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (var client = new ImapClient())
+            {
+                client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+
+                client.Authenticate(UserLogin, UserPassword);
+
+
+                if (FolderList.SelectedItem != null)
+                {
+                    if (FolderList.SelectedItem == FolderList.Items[0])
+                    {
+                        var inbox = client.Inbox;
+                        inbox.Open(FolderAccess.ReadOnly);
+
+                        // Отримання всіх повідомлень з папки "Вхідні"
+                        var uids = inbox.Search(SearchQuery.NotSeen);
+                        MessagesList.Items.Add("Folder");
+                        foreach (var uid in uids)
+                        {
+                            var message = inbox.GetMessage(uid);
+                            MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.Take(10).ToArray())}...");
+                        }
+                    }
+                }
+            }
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
