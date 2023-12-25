1ce352b44c057c90bccb9f06aca2baa9d5c707f9
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2023-12-25 19:56:30 +0100

    finalaized messagesList

--- 03_course_project_SMTP_IMAP/MailSender/MailSender/MailInteface.xaml.cs
+++ 03_course_project_SMTP_IMAP/MailSender/MailSender/MailInteface.xaml.cs
@@ -14,6 +14,14 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using System.Windows.Controls;
+using MailKit;
+using Org.BouncyCastle.Asn1.X509;
+using static MailSender.MailInteface;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Xceed.Wpf.Toolkit;
+using MahApps.Metro.Controls.Dialogs;
+using System.Printing;
 
 namespace MailSender
 {
@@ -24,7 +32,16 @@ namespace MailSender
     {
         private string UserLogin { get; set; }
         private string UserPassword { get; set; }
-        
+
+        List<MainFoledr> folders = new List<MainFoledr> 
+        {
+             new MainFoledr() { Name = "Уся пошта", ServiceName = MailKit.SpecialFolder.All },
+             new MainFoledr() { Name = "Відправлені", ServiceName = MailKit.SpecialFolder.Sent },
+             new MainFoledr() { Name = "Чернетки", ServiceName = MailKit.SpecialFolder.Drafts },
+             new MainFoledr() { Name = "Спам", ServiceName = MailKit.SpecialFolder.Junk },
+             new MainFoledr() { Name = "Видалені", ServiceName = MailKit.SpecialFolder.Trash },
+        };
+
         public MailInteface(string userLogin, string userPassword)
         {
             InitializeComponent();
@@ -38,110 +55,67 @@ namespace MailSender
 
                 client.Authenticate(UserLogin, UserPassword);
 
-                // ------------------------------------------------------------------------------
-
                 // --------------- get all folders
-
-                
-                foreach (var item in client.GetFolders(client.PersonalNamespaces[0]))
+                foreach (var folder in folders)
                 {
-                    FolderList.Items.Add(item.Name);
+                    FolderList.Items.Add(folder.Name);
                 }
-
-                if (FolderList.SelectedItem != null)
-                {
-                    /*
-                    var uids = client.Inbox.Search(SearchQuery.Seen);
-                    MessagesList.Items.Add($"Folder");
-                    foreach (var uid in uids)
-                    {
-                        var message = client.Inbox.GetMessage(uid);
-                        MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.Take(10).ToArray())}...");
-                    }
-                    */
-                    var selectedText = (FolderList.SelectedItem as ListBoxItem).Content.ToString();
-                    MessageBox.Show($"Ви обрали: {selectedText}");
-                }
-
-                // -------------- get all sent messages
-                /*
-                var folder = client.GetFolder(MailKit.SpecialFolder.Sent);
-                folder.Open(FolderAccess.ReadWrite);
-
-                IList<MailKit.UniqueId> uids = folder.Search(SearchQuery.Seen);
-                
-                foreach (var i in uids)
-                {
-                    MimeMessage message = folder.GetMessage(i);
-                    MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.Take(10).ToArray())}...");
-                };
-                */
-
-
-
-                //// -------------------- show Inbox 
-                /*
-                client.Inbox.Open(FolderAccess.ReadOnly);
-
-                Console.WriteLine("--------- Inbox:");
-                foreach (var uid in client.Inbox.Search(SearchQuery.All))
-                {
-                    var m = client.Inbox.GetMessage(uid);
-                    // show message details
-                    Console.WriteLine($"Mail: {m.Subject} - {new string(m.TextBody.Take(10).ToArray())}...");
-                }
-
-                // ---------------------- delete message
-                folder.Open(FolderAccess.ReadWrite);
-                var id = folder.Search(SearchQuery.All).FirstOrDefault();
-                var mail = folder.GetMessage(id);
-
-                Console.WriteLine(mail.Date + " " + mail.Subject);
-
-                folder.AddFlags(new MailKit.UniqueId[] { id }, MessageFlags.Deleted, false);
-
-                folder.Expunge();
-
-                //folder.MoveTo(id, client.GetFolder(SpecialFolder.Junk));  // move to spam
-                //folder.AddFlags(id, MessageFlags.Seen, false);            // mark as read
-                //folder.MoveTo(id, client.GetFolder(SpecialFolder.Trash)); // delete mail
-
-                Console.WriteLine("Press to exit!");
-                Console.ReadKey();
-                */
             }
-            
-
         }
-
-        private void FolderList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public class MainFoledr
         {
-            using (var client = new ImapClient())
+            public string Name { get; set; }
+            public MailKit.SpecialFolder ServiceName { get; set; }
+        }
+        private async void FolderList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessagesList.Items.Clear();
+
+            if (FolderList.SelectedItems != null && FolderList.SelectedItems.Count > 0)
             {
-                client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
-
-                client.Authenticate(UserLogin, UserPassword);
-
-
-                if (FolderList.SelectedItem != null)
+                using (var client = new ImapClient())
                 {
-                    if (FolderList.SelectedItem == FolderList.Items[0])
-                    {
-                        var inbox = client.Inbox;
-                        inbox.Open(FolderAccess.ReadOnly);
+                    client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                    await client.AuthenticateAsync(UserLogin, UserPassword);
 
-                        // Отримання всіх повідомлень з папки "Вхідні"
-                        var uids = inbox.Search(SearchQuery.NotSeen);
-                        MessagesList.Items.Add("Folder");
-                        foreach (var uid in uids)
+                    try
+                    {
+                        foreach (var selectedItem in FolderList.SelectedItems)
                         {
-                            var message = inbox.GetMessage(uid);
-                            MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.Take(10).ToArray())}...");
+                            for (int i = 0; i < FolderList.Items.Count; i++)
+                            {
+                                if (selectedItem == FolderList.Items[i])
+                                {
+                                    var mailFolder = client.GetFolder(folders[i].ServiceName);
+
+                                    // Відкриття папки для читання
+                                    await mailFolder.OpenAsync(FolderAccess.ReadWrite);
+
+                                    // Отримання всіх повідомлень з папки
+                                    var uids = await mailFolder.SearchAsync(SearchQuery.All);
+
+                                    // Додавання назви папки до списку повідомлень
+                                    MessagesList.Items.Add($"Messages in {folders[i].Name}");
+
+                                    // Виведення інформації про кожне повідомлення
+                                    foreach (var uid in uids)
+                                    {
+                                        var message = await mailFolder.GetMessageAsync(uid);
+                                        MessagesList.Items.Add($"{message.Date}: {message.Subject} - {new string(message.TextBody?.ToArray())}...");
+                                    }
+                                }
+                            }
                         }
                     }
+                    catch(Exception ex) 
+                    { 
+                        
+                    }
                 }
             }
         }
+
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -153,6 +127,66 @@ namespace MailSender
             Window1 window1 = new Window1(UserLogin, UserPassword);
             window1.Show();
             this.Close();
+        }
+
+        private async void MessagesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MessagesList.SelectedItems != null && MessagesList.SelectedItems.Count > 0)
+            {
+                // Взяти перший вибраний елемент (якщо є)
+                var selectedMessage = MessagesList.SelectedItem as string;
+
+                if (selectedMessage != null)
+                {
+                    // Припустимо, що дані повідомлення мають фіксований формат для прикладу
+                    string[] messageParts = selectedMessage.Split('-');
+
+                    if (messageParts.Length >= 3)
+                    {
+                        DateTxtBlock.Text = messageParts[0].Trim();
+                        SubjectTxtBlock.Text = messageParts[1].Trim();
+                        MessageTxtBlock.Text = messageParts[2].Trim();
+                    }
+                }
+            }
+        }
+
+        private void CreateNewFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void AddToFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessagesList.SelectedItem != null)
+            {
+                // Отримати вибраний елемент
+                var selectedMessage = MessagesList.SelectedItem as string;
+
+                string choseFolder = ShowInputDialog();
+            }
+            else
+            {
+                MessageBox.Show("Please enter the message.");
+            }
+        }
+        private string ShowInputDialog(object sender, RoutedEventArgs e)
+        {
+            var inputBox = new InputBox("Введення тексту", "Введіть ваш текст:");
+            if (inputBox.ShowDialog() == true)
+            {
+                MessageBox.Show($"Ваш введений текст: {inputBox.Text}");
+            }
+            return inputBox;
+        }
+
+        private void RenameFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ 03_course_project_SMTP_IMAP/MailSender/MailSender/change_folder.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Interaction logic for change_folder.xaml
+    /// </summary>
+    public partial class change_folder : Window
+    {
+        public change_folder()
+        {
+            InitializeComponent();
+        }
+
+        private void OKBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Close();
+        }
+    }
+}
